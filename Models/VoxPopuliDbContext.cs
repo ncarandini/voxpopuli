7f3eeec85803a956241e9a90337338da03ebf68a
enzo9 <enzo.martino.94@gmail.com>
2018-02-15 16:49:33 +0100

    DbContext OnModelCreating Ultimated

--- Models/VoxPopuliDbContext.cs
+++ Models/VoxPopuliDbContext.cs
@@ -15,9 +15,15 @@ namespace voxpopuli.Models
         {
             //Setting Segnalazione's properties
             modelBuilder.Entity<Segnalazione>().Property(t => t.Descrizione).IsRequired();
+            modelBuilder.Entity<Segnalazione>().Property(t => t.DataSegnalazione).IsRequired();
             modelBuilder.Entity<Segnalazione>().HasIndex(t => t.Descrizione);
 
-           
+            //Setting Suggerimento's properties
+            modelBuilder.Entity<Suggerimento>().Property(t => t.Descrizione).IsRequired();
+            modelBuilder.Entity<Suggerimento>().Property(t => t.Voti).IsRequired();
+            modelBuilder.Entity<Suggerimento>().Property(t => t.Titolo).IsRequired();
+            modelBuilder.Entity<Suggerimento>().HasIndex(t => t.Descrizione);
+            modelBuilder.Entity<Suggerimento>().HasIndex(t => t.Titolo);
         }
 
     }
